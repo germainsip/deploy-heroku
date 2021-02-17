c245efa744f8924a61e3ba60f9fdab460fa61f56
Germain SIPIERE <gsipiere@gmail.com>
2021-02-17 17:25:05 +0100

    c'est fini

---
+++ src/Form/YoutubeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Youtube;
+use PhpParser\Node\Stmt\Label;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class YoutubeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url', UrlType::class)
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('Submit', SubmitType::class, ['label' => 'Enregistrer'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Youtube::class,
+        ]);
+    }
+}
