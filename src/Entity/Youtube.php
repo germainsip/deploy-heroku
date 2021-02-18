a24bfb432d3115acd20b43001e2d68edf6d22f1b
Germain SIPIERE <gsipiere@gmail.com>
2021-02-18 18:39:01 +0100

    add api platform

--- src/Entity/Youtube.php
+++ src/Entity/Youtube.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\YoutubeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=YoutubeRepository::class)
+ * @ApiResource
  */
 class Youtube
 {
